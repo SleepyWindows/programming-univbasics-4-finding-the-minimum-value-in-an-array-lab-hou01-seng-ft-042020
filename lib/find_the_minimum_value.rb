cfea30a563d11ec92de4a0ccd79e72e21b372a1a
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-11 19:13:17 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,10 +2,10 @@ def find_min_value(array)
   count = 0
   max = []
   while count < array.length do
+    count += 1
     if array[count] < array.length
       max = array[count]
     end
-    count += 1
   end
   return max
 end
